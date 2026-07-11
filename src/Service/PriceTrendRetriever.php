62565d6e1071bf7c275a848cc02849e85db537f6
Konstantinos Giavridis <constantine_giavridis@hotmail.com>
2026-07-11 21:29:47 +0200

    feat: inject FileReaderRepositoryInterface into PriceTrendRetriever

--- src/Service/PriceTrendRetriever.php
+++ src/Service/PriceTrendRetriever.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Cardmarket\CardmarketClientInterface;
+use App\Infrastructure\FileReaderRepositoryInterface;
 
 class PriceTrendRetriever
 {
     public function __construct(
         private readonly CardmarketClientInterface $cardmarketClient,
+        private readonly FileReaderRepositoryInterface $fileReader,
     ) {
     }
 }
