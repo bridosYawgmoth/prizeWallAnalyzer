e9db669b2ea484d48a76e55c40455326ece0344e
Konstantinos Giavridis <constantine_giavridis@hotmail.com>
2026-07-11 21:44:22 +0200

    feat: inject JsonParserInterface into PriceTrendRetriever

--- src/Service/PriceTrendRetriever.php
+++ src/Service/PriceTrendRetriever.php
@@ -6,12 +6,14 @@ namespace App\Service;
 
 use App\Cardmarket\CardmarketClientInterface;
 use App\Infrastructure\FileReaderRepositoryInterface;
+use App\Infrastructure\JsonParserInterface;
 
 class PriceTrendRetriever
 {
     public function __construct(
         private readonly CardmarketClientInterface $cardmarketClient,
         private readonly FileReaderRepositoryInterface $fileReader,
+        private readonly JsonParserInterface $jsonParser,
     ) {
     }
 }
