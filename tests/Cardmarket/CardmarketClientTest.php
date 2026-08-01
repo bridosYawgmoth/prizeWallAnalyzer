ace284ef20e886753e483fa757561e22bca664e5
Konstantinos Giavridis <constantine_giavridis@hotmail.com>
2026-08-01 12:42:38 +0200

    feat: cover Cardmarket 4xx and 5xx responses as failed product lookups

--- tests/Cardmarket/CardmarketClientTest.php
+++ tests/Cardmarket/CardmarketClientTest.php
@@ -5,9 +5,12 @@ namespace App\Tests\Cardmarket;
 use App\Cardmarket\CardmarketClient;
 use App\Cardmarket\Enum\HttpMethod;
 use App\Cardmarket\OAuth\OAuthSignerInterface;
+use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\Exception\ClientException;
 use Symfony\Component\HttpClient\Exception\JsonException;
+use Symfony\Component\HttpClient\Exception\ServerException;
 use Symfony\Component\HttpClient\Exception\TransportException;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
@@ -84,6 +87,35 @@ final class CardmarketClientTest extends TestCase
             ->expects($this->once())
             ->method('request')
             ->willThrowException(new TransportException('Connection timed out'));
+
+        $this->assertSame([], $this->client->getProduct(productId: 1));
+    }
+
+    #[TestWith(['statusCode' => 404, 'exceptionClass' => ClientException::class], 'Product not found is treated as a failed lookup')]
+    #[TestWith(['statusCode' => 429, 'exceptionClass' => ClientException::class], 'Rate limiting is treated as a failed lookup')]
+    #[TestWith(['statusCode' => 500, 'exceptionClass' => ServerException::class], 'Cardmarket server error is treated as a failed lookup')]
+    #[TestWith(['statusCode' => 503, 'exceptionClass' => ServerException::class], 'Cardmarket outage is treated as a failed lookup')]
+    public function testGetProductReturnsEmptyArrayWhenCardmarketReturnsErrorStatus(int $statusCode, string $exceptionClass): void
+    {
+        $this->signer
+            ->expects($this->once())
+            ->method('sign')
+            ->willReturn('OAuth oauth_consumer_key="testAppToken"');
+
+        $errorResponse = $this->createStub(ResponseInterface::class);
+        $errorResponse->method('getInfo')->willReturnMap([
+            ['http_code', $statusCode],
+            ['url', 'https://apiv2.cardmarket.com/ws/v2.0/output.json/products/1'],
+            ['response_headers', []],
+        ]);
+
+        $response = $this->createStub(ResponseInterface::class);
+        $response->method('toArray')->willThrowException(new $exceptionClass($errorResponse));
+
+        $this->httpClient
+            ->expects($this->once())
+            ->method('request')
+            ->willReturn($response);
 
         $this->assertSame([], $this->client->getProduct(productId: 1));
     }
